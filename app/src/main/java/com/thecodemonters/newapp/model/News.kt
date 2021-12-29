94e9d45316a7e6ff89e9d483b8e1528420fa70cf
Manuel Duarte <carlosd99m@gmail.com>
2021-12-28 22:30:47 -0600

    chore: finish

--- app/src/main/java/com/thecodemonters/newapp/model/News.kt
+++
@@ -1,9 +0,0 @@
-package com.thecodemonters.newapp.model
-
-data class News(
-    var title: String,
-    var content: String?,
-    var author: String?,
-    var url: String,
-    var urlToImage: String,
-)
--- app/src/main/java/com/thecodemonters/newapp/model/NewsApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.thecodemonters.newapp.model
-
-class NewsApiResponse {
-    val status: String? = null
-    val code: String? = null
-    val articles: List<News>? = null
-}
